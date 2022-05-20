09f3a6bc5b2e082fb6d6a9a40e8e34c3c53ecb8e
Oluwafemi Otusanya <femiot@hotmail.com>
2022-05-20 19:03:23 +0200

    Updates to generic repository

--- src/Insurance.Infrastructure/EF/Repositories/GenericRepository.cs
+++ src/Insurance.Infrastructure/EF/Repositories/GenericRepository.cs
@@ -16,24 +16,9 @@ namespace Insurance.Infrastructure.Repositories
             _dbSet = context.Set<TEntity>();
         }
 
-        public async Task<TEntity?> FirstOrDefaultAsync()
-        {
-            return await _dbSet.FirstOrDefaultAsync();
-        }
-
-        public TEntity? FirstOrDefault()
-        {
-            return _dbSet.FirstOrDefault();
-        }
-
         public async Task<TEntity?> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate)
         {
             return await _dbSet.FirstOrDefaultAsync(predicate);
-        }
-
-        public TEntity? FirstOrDefault(Expression<Func<TEntity, bool>> predicate)
-        {
-            return _dbSet.FirstOrDefault(predicate);
         }
 
         public async Task<IEnumerable<TEntity>?> FindAsync(Expression<Func<TEntity, bool>> predicate)
@@ -41,65 +26,9 @@ namespace Insurance.Infrastructure.Repositories
             return await Task.Run(() => _dbSet.AsNoTracking().Where(predicate));
         }
 
-        public async Task<List<TEntity>> GetListAsync()
-        {
-            return await _dbSet.ToListAsync();
-        }
-
-        public IQueryable<TEntity> AsQueryable()
-        {
-            return _dbSet.AsQueryable();
-        }
-
-        public IQueryable<TEntity> Include(string[] properties)
-        {
-           var queryable = _dbSet.AsQueryable();
-            foreach (var property in properties)
-                queryable = queryable.Include(property);
-
-            return queryable;
-        }
-
-        public async Task<List<TEntity>> GetListAsync(Expression<Func<TEntity, bool>> predicate)
-        {
-            return await _dbSet.Where(predicate).ToListAsync();
-        }
-
-        public async Task<int> CountAsync(Expression<Func<TEntity, bool>> predicate)
-        {
-            return await _dbSet.CountAsync(predicate);
-        }
-
-        public async Task<int> CountAsync()
-        {
-            return await _dbSet.CountAsync();
-        }
-
         public async Task InsertAsync(TEntity entity)
         {
             await _dbSet.AddAsync(entity);
-        }
-        public async Task InsertRangeAsync(List<TEntity> entities)
-        {
-            await _dbSet.AddRangeAsync(entities);
-        }
-
-        public virtual void Delete(TEntity entityToDelete)
-        {
-            if (_insuranceContext.Entry(entityToDelete).State == EntityState.Detached)
-            {
-                _dbSet.Attach(entityToDelete);
-            }
-            _dbSet.Remove(entityToDelete);
-        }
-
-        public virtual void Delete(IEnumerable<TEntity> entitiesToDelete)
-        {
-            if (_insuranceContext.Entry(entitiesToDelete).State == EntityState.Detached)
-            {
-                _dbSet.AttachRange(entitiesToDelete);
-            }
-            _dbSet.RemoveRange(entitiesToDelete);
         }
 
         public virtual void Update(TEntity entityToUpdate)
